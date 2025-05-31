616c88c16deaba172ef00edbc353a1e58592e1b8
Rolands Kundzins <rolis.kungs@inbox.lv>
2025-05-31 14:49:43 +0300

    add prettier to eslint - show up in vscode

--- nuxt/eslint.config.mjs
+++ nuxt/eslint.config.mjs
@@ -1,6 +1,16 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from "./.nuxt/eslint.config.mjs";
+import prettierPlugin from "eslint-plugin-prettier";
 
-export default withNuxt(
-  // Your custom configs here
-)
+export default withNuxt([
+  // ...other configs,
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      // @ts-ignore
+      ...prettierPlugin.configs.recommended.rules,
+    },
+  },
+]);
