22e4e45956639c0028201b427476afcf4e4b463b
Rolands Kundzins <rolis.kungs@inbox.lv>
2025-06-10 06:47:52 +0300

    Other files

---
+++ nuxt_frontend/components/MessageComponent.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Message from Python Backend:</h1>
+    <h2>{{ message }}</h2>
+  </div>
+</template>
+
+<script setup>
+const { data } = useFetch("/api/message", { server: false });
+const message = computed(() => data.value?.message ?? "Loading...");
+</script>
